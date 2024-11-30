349a5f34ff98bc8c7d09cc3fb7398304d211f8fb
virginiadadamo <virginia.dadamo@epfl.ch>
2024-11-30 09:42:14 +0100

    jobnoB

--- MC_simulation.py
+++ MC_simulation.py
@@ -24,8 +24,7 @@ ASSUMPTIONS:
 
 alfa = 0.7 #ratio between nA/N 
 N = 3000 #total number of binding sites in the DNA
-#nA = int (N*alfa) #number of As
-nA = 100
+nA = int (N*alfa) #number of As
 
 
 nB = 1#100 #500 #number of Bs 
