2718e5742789aaa2d87e9ada2732704ca7375f96
virginiadadamo <virginia.dadamo@epfl.ch>
2024-12-07 19:58:41 +0100

    0.4_K_10

--- MC_simulation.py
+++ MC_simulation.py
@@ -30,7 +30,7 @@ nA = int (N*alfa) #number of As
 
 
 nB = 100 #number of Bs 
-k = 5#10 #, 5, 10]  #number of B interacting sites with A  
+k = 10 #10 #, 5, 10]  #number of B interacting sites with A  
 #beta fraction B over As
 #Adding protein B in the simulation (True if you want to add, False otherwise)
 protein_B= True    
